adc6c64c5cabfd55f522b83b376dff0ac0bfd3f1
oscar02r <oscary.ramirez@gmail.com>
2020-05-05 21:39:24 +0200

    Creatin form

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,9 +1,47 @@
-import React from 'react'
+import React from "react";
 
 const Login = () => {
-    return (
-       <h1>Desde login</h1> 
-    );
-}
+  const onChange = (e) => {};
+
+  return (
+    <div className="form-usuario">
+      <div className="contenedor-form sombra-dark">
+        <h1>Iniciar Sesión </h1>
+        <form>
+          <div className="campo-form">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              className=""
+              id="email"
+              name="email"
+              placeholder="Tu Email"
+              onChange={onChange}
+            />
+          </div>
+          <div className="campo-form">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              className=""
+              id="password"
+              name="password"
+              placeholder="Password"
+              onChange={onChange}
+            />
+          </div>
+          <div className="campo-form">
+            <input 
+              type="submit" 
+              className="btn btn-primario btn-block" 
+              onChange={onChange}
+              value="Iniciar Sesión"
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
